2e46da436bb69f730d4d55862939e5474c8c0183
Hongyi-Luu <HONGYI.Luu@gmail.com>
2024-05-16 17:13:10 +0100

    'process_data.py' and 'train_classifier.py' completed

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,79 @@
 import sys
 
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+nltk.download('omw-1.4') 
+import pandas as pd
+from sqlalchemy import create_engine
+import numpy as np
+from scipy.stats import gmean
+import os
+import re
+import pickle
+from sqlalchemy import create_engine
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import fbeta_score, make_scorer
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.base import BaseEstimator,TransformerMixin
+from xgboost import XGBClassifier
 
-def load_data(database_filepath):
-    pass
-
+def load_data(database_filepath): 
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = df = pd.read_sql_table('DisasterResponse', engine)
+    X = df['message']
+    Y = df.iloc[:,4:]
+    category_names = Y.columns
+    return X, Y, category_names
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    
+    for detected_url in detected_urls:
+        text = text.replace(detected_url, "urlplaceholder")
 
+    tokens = nltk.word_tokenize(text) # Extract the word tokens
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = [lemmatizer.lemmatize(w).lower().strip() for w in tokens]
+    
+    return clean_tokens
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    parameters = {
+        'clf__estimator__n_estimators': [100],
+        'clf__estimator__min_samples_split': [2]
+    }
+    model = GridSearchCV(pipeline, param_grid=parameters, 
+                         verbose=2, n_jobs=-1, cv=3)
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    for i, col in enumerate(category_names):
+        print(f'{col}\n', 
+              classification_report(Y_test[col], y_pred[:, i], zero_division=0))
 
 
 def save_model(model, model_filepath):
-    pass
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
@@ -46,7 +101,7 @@ def main():
         print('Please provide the filepath of the disaster messages database '\
               'as the first argument and the filepath of the pickle file to '\
               'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+              'train_classifier.py ../data/DisasterResponse.db MLclassifier.pkl')
 
 
 if __name__ == '__main__':
